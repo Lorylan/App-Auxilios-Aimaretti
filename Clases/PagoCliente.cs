844b86f55aa174f74340e45dc25e4ca372c3a718
mili.aylen.j@gmail.com <Lorylan@DESKTOP-1DI1S74>
2020-11-21 17:48:58 -0300

    Agrega(): Agrega clase que representa el pago de un cliente

--- Clases/PagoCliente.cs
+++ Clases/PagoCliente.cs
@@ -8,6 +8,31 @@ namespace PuebaDeDiseñoAA.Clases
 {
     class PagoCliente
     {
+        public String ComprobanteNro { get; set; }
+        public DateTime FechaC { get; set; }
+        public String Monto { get; set; }
+        public String Nro { get; set; }
+        public String Banco { get; set; }
+        public String NroCuenta { get; set; }
+        public DateTime Fecha { get; set; }
+        public String Detalle { get; set; }
+        public String idCliente { get; set; }
+        public String idUsuario { get; set; }
+
+
+        public PagoCliente(String com_nro, DateTime fc, String m, String nro, String b,String nro_c,
+            DateTime fecha, String d, String idC) {
+            idUsuario = Program.nombreUsuario;
+            idCliente = idC;
+            Detalle = d;
+            Fecha = fecha;
+            NroCuenta = nro_c;
+            Banco = b;
+            Nro = nro;
+            Monto = m;
+            FechaC = fc;
+            ComprobanteNro = com_nro;
+        }
 
     }
 }
